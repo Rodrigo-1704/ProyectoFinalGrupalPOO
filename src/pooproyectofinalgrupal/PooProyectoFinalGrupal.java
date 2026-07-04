bec210330d2069b5b67c3f6ef5a3ddf6d4cbfdf7
Rodrigo-1704 <20236791@aloe.ulima.edu.pe>
2026-07-03 20:49:28 -0500

    cambios textos

--- src/pooproyectofinalgrupal/PooProyectoFinalGrupal.java
+++ src/pooproyectofinalgrupal/PooProyectoFinalGrupal.java
@@ -14,7 +14,10 @@ public class PooProyectoFinalGrupal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic he
     }
     
 }
+
+
+//prueba cambio 111

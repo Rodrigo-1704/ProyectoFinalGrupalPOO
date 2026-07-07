7d14a5a7f49d0584cc5acda6944ffb8dff72914f
20243574 <20243574@aloe.ulima.edu.pe>
2026-07-07 18:20:07 -0500

    Update Administrador.java

--- src/Clases/Administrador.java
+++ src/Clases/Administrador.java
@@ -4,22 +4,18 @@
  */
 package Clases;
 
-public class Administrador extends Empleados {
+public class Administrador extends Empleado {
     public Administrador(String dni, String nombres, String apellidos, String usuario, String contraseña){
         super(dni, nombres, apellidos, usuario, contraseña, "Administrador");
 }
-    public String gestionarSistema() {
-        return "El administrador está gestionando el sistema.";
-}
+    @Override
+    public void realizarTrabajo() {
+        System.out.println("Gestionando el sistema...");
 
     @Override
     public String mostrarInformacion() {
         return super.mostrarInformacion() +
                 "\nFunción: Gestionar usuarios, clientes, reservas y ventas.";
- }
-     @Override
- public void realizarTrabajo() {
-     System.out.println("Gestionando el sistema...");
  }
 
 }

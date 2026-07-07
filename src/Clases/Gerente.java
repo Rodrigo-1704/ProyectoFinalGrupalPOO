867363244b52da8e60126c39b1f602aadd54c984
20243574 <20243574@aloe.ulima.edu.pe>
2026-07-07 01:12:42 -0500

    Update Gerente.java

--- src/Clases/Gerente.java
+++ src/Clases/Gerente.java
@@ -4,10 +4,18 @@
  */
 package Clases;
 
-/**
- *
- * @author rod17
- */
 public class Gerente {
-    
+    public Gerente(String dni, String nombres, String apellidos, String usuario, String contrasena) {
+        super(dni, nombres, apellidos, usuario, contrasena, "Gerente"); 
 }
+ public String revisarEstadisticas(){
+     return "El gerente está revisando las estadísticas del sistema.";
+}
+
+@Override 
+public String mostrarInformacion() {
+    return super.mostrarInformacion() +
+        "\nFunción: Revisar estadísticas y reportes.";
+}
+}
+ 

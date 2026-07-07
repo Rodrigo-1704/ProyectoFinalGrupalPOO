4f9d51917f3752a1bdf6f462bdd92576a81f3e40
20243574 <20243574@aloe.ulima.edu.pe>
2026-07-07 00:46:21 -0500

    Update Empleados.java

--- src/Clases/Empleados.java
+++ src/Clases/Empleados.java
@@ -4,10 +4,79 @@
  */
 package Clases;
 
-/**
- *
- * @author rod17
- */
+
 public class Empleados {
+    private String dni;
+    private String nombres;
+    private String apellidos;
+    private String usuario;
+    private String contrasena;
+    private String rol;
+   
+    public Empleado(String dni, String nombres, String apellidos, String usuario, String contrasena, String rol) {
+        this.dni = dni;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.rol = rol;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    public String mostrarInformacion() {
+        return "DNI :" + dni +
+               "Nombres :" + apellidos +
+               "Usuario :" + usuario +
+               "Rol :" + rol;
+        
+            
+            
+
     
 }

a89dcfa9728325806e3843ff09051cb5e6b7b7ea
20243574 <20243574@aloe.ulima.edu.pe>
2026-07-07 23:45:44 -0500

    Update Administrador.java

--- src/Clases/Administrador.java
+++ src/Clases/Administrador.java
@@ -1,25 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Clases;
 
 public class Administrador extends Empleado {
-    public Administrador(String dni, String nombres, String apellidos, String usuario, String contraseña){
-        super(dni, nombres, apellidos, usuario, contraseña, "Administrador");
-}
-    @Override
-    public void realizarTrabajo() {
-        System.out.println("Gestionando el sistema...");
-}
-    public String gestionarUsuarios() {
-        return "El administrador puede registrar, modificar y eliminar usuarios.";
-}
 
+    public Administrador(String dni, String nombres, String apellidos, String usuario, String contrasena, String rol) {
+        super(dni, nombres, apellidos, usuario, contrasena, "Administrador");
+    }
+    @Override
+    public String obtenerPermisos(){
+        return "Gestiona empleados, proyectos y acabados.";
+        
+    }
     @Override
     public String mostrarInformacion() {
         return super.mostrarInformacion() +
-                "\nFunción: Gestionar usuarios, clientes, reservas y ventas.";
- }
-
+                "\nPermisos: " + obtenerPermisos();
+    }
 }

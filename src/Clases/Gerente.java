d896182b3a5257f47f502bd2f7e2c4c2074e1512
20243574 <20243574@aloe.ulima.edu.pe>
2026-07-07 19:52:33 -0500

    Update Gerente.java

--- src/Clases/Gerente.java
+++ src/Clases/Gerente.java
@@ -5,19 +5,42 @@
 package Clases;
 
  public class Gerente extends Empleado {
-     public Gerente(String dni, String nombres, String apellidos, String usuario, String contrasena) {
-         super(dni, nombres, apellidos, usuario, contrasena, "Gerente"); 
- }
-  @Override
-  public void realizarTrabajo() {
-      System.out.println("Revisando estadísticas...");
-}
- 
+     private String area;
+     private String bono;
 
- @Override 
- public String mostrarInformacion() {
-     return super.mostrarInformacion() +
-         "\nFunción: Revisar estadísticas y reportes.";
- }
+     public Gerente (String dni, String nombres, String apellidos, String Usuario, String contrasena, String area, String bono) {
+      super (dni, nombres, apellidos, usuario, contrasena, "Gerente");
+
+      this.area = area;
+      this.bono = bono;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public double getBono() {
+        return bono;
+    }
+
+    public void setBono(double bono) {
+        this.bono = bono;
+    }
+
+    @Override
+    public void realizarTrabajo() {
+        System.out.println("El gerente supervisa las operaciones.");
+    }
+
+    @Override
+    public String mostrarInformacion() {
+        return super.mostrarInformacion()
+                + "\nÁrea: " + area
+                + "\nBono: S/ " + bono;
+    }
 }
  

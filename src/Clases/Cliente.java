c01301ded19731b30d085ce40199e8f91d9e9d9a
20243574 <20243574@aloe.ulima.edu.pe>
2026-07-07 23:48:40 -0500

    Update Cliente.java

--- src/Clases/Cliente.java
+++ src/Clases/Cliente.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Clases;
 
 public class Cliente {
@@ -12,46 +8,92 @@ public class Cliente {
     private String fechaNacimiento;
     private String estadoCivil;
     private String ocupacion;
-    private double ingresos;
+    private double ingresosMensuales;
     private String telefono;
     private String correo;
 
-    public Cliente(String dni, String nombres, String apellidos, String fechaNacimiento,
-                   String estadoCivil, String ocupacion, double ingresos, String telefono, String correo) {
+    public Cliente(String dni, String nombres, String apellidos, String fechaNacimiento, String estadoCivil, String ocupacion, double ingresosMensuales, String telefono, String correo) {
         this.dni = dni;
         this.nombres = nombres;
         this.apellidos = apellidos;
         this.fechaNacimiento = fechaNacimiento;
         this.estadoCivil = estadoCivil;
         this.ocupacion = ocupacion;
-        this.ingresos = ingresos;
+        this.ingresosMensuales = ingresosMensuales;
         this.telefono = telefono;
         this.correo = correo;
-}
-     public String getDni() {
+    }
+
+    public String getDni() {
         return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
     }
 
     public String getNombres() {
         return nombres;
     }
 
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
     public String getApellidos() {
         return apellidos;
     }
 
-    public double getIngresos() {
-        return ingresos;
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
     }
-    public String mostrarInformacion() {
-        return "DNI : " + dni +
-               "\nCliente: " + nombres + " " + apellidos +
-               "\nFecha de nacimiento : " + fechaNacimiento +
-               "\nEstado civil: " + estadoCivil +
-               "\nOcupación : " + ocupacion +
-               "\nIngresos: S/ " + ingresos +
-                "\nTeléfono: " + telefono +
-                "\nCorreo: " + correo;
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getOcupacion() {
+        return ocupacion;
+    }
+
+    public void setOcupacion(String ocupacion) {
+        this.ocupacion = ocupacion;
+    }
+
+    public double getIngresosMensuales() {
+        return ingresosMensuales;
+    }
+
+    public void setIngresosMensuales(double ingresosMensuales) {
+        this.ingresosMensuales = ingresosMensuales;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
     }
 }
 
